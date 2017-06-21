e0fbdba88e1728dc6c22c70fb82479796717b965
nicholas hays <nicholas.hays@hotmail.com>
2017-06-20 18:07:01 -0700

    scanned input

--- buildingbridges.js
+++ buildingbridges.js
@@ -1,24 +1,51 @@
 
 
-function main() {
+function main(input) {
 
 var queue = [];
 
 var map = {};
 
-var input = [];
+var arr = [];
 
+var data = input.splice("\n");
 
+var index = 0;
 
+while (true) {
 
+	var dims = data[index++].splice(" ");
 
+	var rows = dims[0];
 
+	var cols = dims[1];
 
+	if(rows == 0 && cols == 0)
+		 break;
+
+	for ( var i = 0; i < rows; i++) {
+
+		arr[0] = [];
+
+		var str = data[index++];
+
+		for(var j = 0; j < cols; j++) {
+
+			arr[i][j] = str[j];
+
+		}
+	}
+
+}
+
+genBuildings(arr);
+search();
+buildBridges();
 
 }
 
 
-function genBuildings() {
+function genBuildings(arr) {
 
 }
 
