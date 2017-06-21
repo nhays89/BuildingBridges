5c31f8722c4c1ce864d17d951ed8715a93a7c19c
nicholas hays <nicholas.hays@hotmail.com>
2017-06-20 17:46:44 -0700

    init commit

---
+++ buildingbridges.js
@@ -0,0 +1,38 @@
+
+
+function main() {
+
+var queue = [];
+
+var map = {};
+
+var input = [];
+
+
+
+
+
+
+
+
+}
+
+
+function genBuildings() {
+
+}
+
+
+function search() {
+
+}
+
+function buildBridges() {
+
+}
+
+
+
+main("3 5\n#...#\n..#..\n#...#\n3 5\n##...\n.....\n....#\n3 5\n#.###\n#.#.#\n###.#\n3 5\n#.#..\n.....\n....#\n0 0");
+
+
